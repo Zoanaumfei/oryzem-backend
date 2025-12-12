138a004c2222585f02947a60f39a7c9999672463
Zoanaumfei <vanderson.verza@gmail.com>
2025-12-12 17:47:48 -0300

    docker permission

---
+++ src/main/java/com/oryzem/backend/config/CorsConfig.java
@@ -0,0 +1,30 @@
+package com.oryzem.backend.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/api/**")  // Aplica a todos endpoints /api/*
+                        .allowedOrigins(
+                                "https://oryzem.com",       // Seu domínio principal
+                                "https://www.oryzem.com"   // Com www
+
+                        )
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true)
+                        .maxAge(3600);  // Cache de pré-voo por 1 hora
+            }
+        };
+    }
+}
